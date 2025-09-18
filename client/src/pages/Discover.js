73a1dfce8601dec164c6ef8af8c67c447e0aeeec
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-09-18 17:46:18 -0400

    Added a sentence to the discover page for testing.

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -1,5 +1,10 @@
 // Discover.js: Placeholder for event discovery page
 import React from 'react';
 export default function Discover() {
-  return <div>Discover Events</div>;
+  return (
+    <div>
+      <h1>Discover Events</h1>
+      <p>testing!</p>
+    </div>
+  );
 }
