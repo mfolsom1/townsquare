9a2e6ba81a9a47bca03b02f3fc8ba245529130f7
vijjeara long <vijjearalong@ufl.edu>
2025-09-22 16:35:24 -0400

    Creation and style of Navigation Bar/Create Event modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 // App.js: Handles routing
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Discover from './pages/Discover';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Discover from "./pages/Discover";
+
+function Following() {
+  return <h1>Following Page</h1>;
+}
+
+function Saved() {
+  return <h1>Saved Events Page</h1>;
+}
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Discover />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/following" element={<Following />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="*" element={<Discover />} />
       </Routes>
     </Router>
   );
