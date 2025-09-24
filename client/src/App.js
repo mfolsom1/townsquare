19e0fdc8429c444b25672ee304bf515c497e1856
vijjeara long <vijjearalong@ufl.edu>
2025-09-23 20:11:12 -0400

    add auth to app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Discover from "./pages/Discover";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import RequireAuth from "./auth/RequireAuth";
+import { AuthProvider } from "./auth/AuthContext";
 
 function Following() {
   return <h1>Following Page</h1>;
@@ -14,15 +18,31 @@ function Saved() {
 
 function App() {
   return (
+    <AuthProvider>
     <Router>
-      <NavBar />
       <Routes>
-        <Route path="/discover" element={<Discover />} />
-        <Route path="/following" element={<Following />} />
-        <Route path="/saved" element={<Saved />} />
-        <Route path="*" element={<Discover />} />
-      </Routes>
-    </Router>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route
+            path="/*"
+            element={
+              <RequireAuth>
+                <>
+                <NavBar />
+                <Routes>
+                  <Route path="/discover" element={<Discover />} />
+                  <Route path="/following" element={<Following />} />
+                  <Route path="/saved" element={<Saved />} />
+                  <Route path="*" element={<Discover />} />
+                </Routes>
+              </>
+            </RequireAuth>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
