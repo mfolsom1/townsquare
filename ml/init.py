4af92c6748e6af51985a460474be9f3631d8316b
mfolsom1 <100625046+mfolsom1@users.noreply.github.com>
2025-10-17 21:04:02 -0400

    Friend recs + training flow

--- ml/init.py
+++ ml/init.py
@@ -6,12 +6,23 @@ __author__ = "Townsquare Team"
 
 from .recommend import RecommendationEngine, RecommendationAPI
 from .train import ModelTrainer
-from .utils import DatabaseConnector, EmbeddingGenerator
+from .utils import (
+    DatabaseConnector,
+    EmbeddingGenerator,
+    VectorStore,
+    TextPreprocessor,
+    DataValidator,
+    get_interaction_weight,
+)
 
 __all__ = [
-    'RecommendationEngine',
-    'RecommendationAPI', 
-    'ModelTrainer',
-    'DatabaseConnector',
-    'EmbeddingGenerator'
+    "RecommendationEngine",
+    "RecommendationAPI",
+    "ModelTrainer",
+    "DatabaseConnector",
+    "EmbeddingGenerator",
+    "VectorStore",
+    "TextPreprocessor",
+    "DataValidator",
+    "get_interaction_weight",
 ]
