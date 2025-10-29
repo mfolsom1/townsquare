a37306f117fff95147d3684923f1a31fdd6844d2
derek-mo <derek.mo7203@gmail.com>
2025-10-29 17:54:41 -0400

    addcards

--- client/src/pages/Following.jsx
+++ client/src/pages/Following.jsx
@@ -1,8 +1,54 @@
-import React from 'react';
-import './Following.css'
+import React, { useState, useEffect } from 'react';
+import './Following.css';
+import { getEvents } from '../api';
+import SavedEvents from "../hooks/SavedEvents";
 import EventCard from '../components/EventCard';
+import { redirect } from 'react-router-dom';
 
 export default function Following() {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { isSaved, toggleSaved } = SavedEvents();
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await getEvents();
+            // We convert the object of events into an array
+            const eventsArray = Object.values(response.events || {});
+            setEvents(eventsArray);
+        } catch (err) {
+            setError(err.message || "An unexpected error occurred.");
+        } finally {
+            setLoading(false);
+        }
+      
+    };
+    fetchEvents();
+  }, []);
+
+  const renderContent = () => {
+      if (loading) return <p className="ts-loading">Loading events...</p>;
+      if (error) return <p className="ts-error">Error: {error}</p>;
+      if (events.length === 0) return <p>No upcoming events found. Why not create one?</p>;
+
+      return (
+          <div className="event-grid">
+                {events.map((event) => (
+                  <EventCard
+                    key={event.event_id}
+                    event={event}
+                    isSaved={isSaved}
+                    onToggleSaved={toggleSaved}
+                  />
+                ))}
+          </div>
+      );
+  };
+
   return (
     <main className='main-container'>
       <div>
@@ -14,7 +60,7 @@ export default function Following() {
       <div>
         <h2 className='friends-header'>Events with People You Follow</h2>
         <div>
-          Cards
+          {renderContent()}
         </div>
       </div>
 
@@ -22,7 +68,7 @@ export default function Following() {
       <div>
         <h2 className='orgs-header'>Events from Organizations You Follow</h2>
         <div>
-          Cards
+          {renderContent()}
         </div>
       </div>
     </main>
